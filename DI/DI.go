e7d50d04dff96846fbb58b3febd0008c3fe9177c
bordunos <bordunos.p@gmail.com>
2023-02-05 22:27:49 +0200

    IDisposable

--- DI/DI.go
+++ DI/DI.go
@@ -2,6 +2,7 @@ package DI
 
 import (
 	"errors"
+	"github.com/bordunosp/ddd"
 	"sync"
 )
 
@@ -9,12 +10,21 @@ var ErrServiceNotRegistered = errors.New("service not registered")
 var ErrServiceHasIncorrectType = errors.New("service has incorrect type")
 var ErrServiceAlreadyRegistered = errors.New("service already registered")
 
-var registeredServices = sync.Map{}
+var registeredServices = struct {
+	mu    sync.RWMutex
+	items map[string]any
+}{
+	mu:    sync.RWMutex{},
+	items: make(map[string]any),
+}
 
 func Get[T any](serviceName string) (T, error) {
+	registeredServices.mu.RLocker()
+	defer registeredServices.mu.RUnlock()
+
 	var serviceT T
 
-	service, ok := registeredServices.Load(serviceName)
+	service, ok := registeredServices.items[serviceName]
 	if !ok {
 		return serviceT, ErrServiceNotRegistered
 	}
@@ -42,8 +52,11 @@ func Get[T any](serviceName string) (T, error) {
 }
 
 func RegisterServices(services []ServiceItem) error {
+	registeredServices.mu.Lock()
+	defer registeredServices.mu.Unlock()
+
 	for _, service := range services {
-		if _, ok := registeredServices.Load(service.ServiceName); ok {
+		if _, ok := registeredServices.items[service.ServiceName]; ok {
 			return ErrServiceAlreadyRegistered
 		}
 
@@ -52,11 +65,28 @@ func RegisterServices(services []ServiceItem) error {
 			if err != nil {
 				return err
 			}
-			registeredServices.Store(service.ServiceName, instance)
+			registeredServices.items[service.ServiceName] = instance
 		} else {
-			registeredServices.Store(service.ServiceName, service.ServiceInitFunc)
+			registeredServices.items[service.ServiceName] = service.ServiceInitFunc
 		}
 	}
 
 	return nil
+}
+
+func Dispose() {
+	registeredServices.mu.Lock()
+	defer registeredServices.mu.Unlock()
+
+	for _, service := range registeredServices.items {
+		if _, ok := service.(ServiceInitFunc); ok {
+			continue
+		}
+
+		if disposableService, ok := service.(ddd.IDisposable); ok {
+			disposableService.Dispose()
+		}
+	}
+
+	registeredServices.items = make(map[string]any)
 }
---
+++ IDisposable.go
@@ -0,0 +1,5 @@
+package ddd
+
+type IDisposable interface {
+	Dispose()
+}
